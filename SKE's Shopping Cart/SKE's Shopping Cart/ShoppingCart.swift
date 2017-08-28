ae37f7cac3ff22752c055545899e8ccfe8b59697
SKE <appybuildmore@gmail.com>
2017-08-27 22:59:30 -0500

    1. Added data model and class function 2. Added some code to the ShoppingCartViewController to update info when view loads.

---
+++ SKE's Shopping Cart/SKE's Shopping Cart/ShoppingCart.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCart.swift
+//  SKE's Shopping Cart
+//
+//  Created by Sain-R Edwards Jr. on 8/27/17.
+//  Copyright © 2017 Appybuildmore. All rights reserved.
+//
+
+import UIKit
+
+struct ShoppingCart {
+    var image: UIImage
+    var title: String
+    var price: Int
+    var quantity: Int
+}
+
